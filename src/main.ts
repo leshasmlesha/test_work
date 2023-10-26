b08b767a161a236255046202a7f763f240d25ce2
alex_dev_sm <alex_dev_sm@ALEX.USB.RU>
2023-10-26 20:02:18 +0300

    feat(server): запущен сервер

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-async function bootstrap() {}
-bootstrap()
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  await app.listen(8080, '0.0.0.0');
+  console.log(`Сервер запущен на ${await app.getUrl()}`);
+}
+bootstrap();
